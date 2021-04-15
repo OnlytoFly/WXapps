ba3b815e89e752ab5982c48ab08d3417d95746b3
OnlytoFly <1350082833@qq.com>
2021-04-15 14:18:33 +0800

    2021.4.15afternoon

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,3 +1,5 @@
+import {request} from "../../request/index.js"
+import {regeneratorRuntime} from "../../lib/runtime/runtime"
 // pages/mine/mine.js
 Page({
 
@@ -5,7 +7,13 @@ Page({
    * 页面的初始数据
    */
   data: {
+    test:[]
 
+  },
+  loginParams:{
+    encryptedData:"",
+    iv:"",
+    code:""
   },
 
   /**
@@ -13,8 +21,34 @@ Page({
    */
   onLoad: function (options) {
 
-  },
+    var thisblock=this;
 
+    wx.login({
+      success: function (res) {
+        thisblock.loginParams.code=res.code;
+      }
+     })
+  },
+  async getAppsList(){
+    const res=await request({url:"wxuserinfo/id",data:this.loginParams});
+    this.setData({
+      test:res
+    })
+    //console.log(this.data.code);
+    console.log(res);
+    
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.loginParams.encryptedData=res.encryptedData;
+        this.loginParams.iv=res.iv;
+        console.log(this.loginParams);
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
