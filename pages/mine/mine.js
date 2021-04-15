fd6f5424145680f824aba5a365c0abc077bc78db
OnlytoFly <1350082833@qq.com>
2021-04-15 18:15:31 +0800

    2021.4.15night

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,31 +1,25 @@
+
 import {request} from "../../request/index.js"
 import {regeneratorRuntime} from "../../lib/runtime/runtime"
 // pages/mine/mine.js
-Page({
 
-  /**
-   * 页面的初始数据
+Page({
+   /* 页面的初始数据
    */
   data: {
     test:[]
-  },
 
-  loginParams:{
+  },
+  loginParams: {
     encryptedData:"",
     iv:"",
     code:""
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
     var thisblock=this;
 
@@ -42,8 +36,8 @@ Page({
     })
     //console.log(this.data.code);
     console.log(res);
-
-  },
+    
+  }, 
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
@@ -51,50 +45,8 @@ Page({
       success: (res) => {
         this.loginParams.encryptedData=res.encryptedData;
         this.loginParams.iv=res.iv;
-        console.log(this.loginParams);
+        //console.log(this.loginParams);
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
